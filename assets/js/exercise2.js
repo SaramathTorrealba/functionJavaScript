bd34e25755ebda064caba84724c0f48e18cd8816
SaramathTorrealba <saraaechaxjc@gmail.com>
2022-07-08 15:34:26 -0400

    add function in js

--- assets/js/exercise2.js
+++ assets/js/exercise2.js
@@ -6,8 +6,13 @@ function tickets(){
 ticketsValue= Number(document.querySelector('#value1').value)+
               Number(document.querySelector('#value2').value)+
               Number(document.querySelector('#value3').value);
-
-if (ticketsValue == 0) {
+let input1=document.querySelector('#value1').value
+let input2=document.querySelector('#value2').value
+let input3=document.querySelector('#value3').value
+if (isNaN(input1) || isNaN(input2) || isNaN(input3)){
+ message.innerHTML="Sólo valores númericos"
+}
+else if (ticketsValue == 0) {
     message.innerHTML= "no has seleccionado ningun sticker"
 }
 else if (ticketsValue >10) {
